55ea1d5b4b8e0c7978198e885e25a45dec73610d
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-12-09 13:01:48 +0100

    Default ICombinatorSettings implementation

---
+++ Models/CombinatorSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Piedone.Combinator.Models
+{
+    public class CombinatorSettings : ICombinatorSettings
+    {
+        public string CombinationExcludeRegex { get; set; }
+        public bool CombineCDNResources { get; set; }
+        public bool MinifyResources { get; set; }
+        public string MinificationExcludeRegex { get; set; }
+    }
+}

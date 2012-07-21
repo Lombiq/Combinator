dc2c199da7aaa26b04a6269e5f8e5aaafd60a0bd
Piedone <piedone@pyrocenter.hu>
2012-07-21 11:06:21 +0200

    Adding StringLengthMax attributes to string record properties

--- Models/CombinatorSettingsPartRecord.cs
+++ Models/CombinatorSettingsPartRecord.cs
@@ -1,19 +1,37 @@
 ﻿using Orchard.ContentManagement.Records;
 using Orchard.Environment.Extensions;
+using Orchard.Data.Conventions;
 
 namespace Piedone.Combinator.Models
 {
     [OrchardFeature("Piedone.Combinator")]
     public class CombinatorSettingsPartRecord : ContentPartRecord
     {
+        [StringLengthMax]
         public virtual string CombinationExcludeRegex { get; set; }
+
+        [StringLengthMax]
         public virtual bool CombineCdnResources { get; set; }
+
+        [StringLengthMax]
         public virtual bool MinifyResources { get; set; }
+
+        [StringLengthMax]
         public virtual string MinificationExcludeRegex { get; set; }
+
+        [StringLengthMax]
         public virtual bool EmbedCssImages { get; set; }
+
+        [StringLengthMax]
         public virtual int EmbeddedImagesMaxSizeKB { get; set; }
+
+        [StringLengthMax]
         public virtual string EmbedCssImagesStylesheetExcludeRegex { get; set; }
+
+        [StringLengthMax]
         public virtual string ResourceSetRegexes { get; set; }
+
+        [StringLengthMax]
         public virtual bool EnableForAdmin { get; set; }
 
         public CombinatorSettingsPartRecord()

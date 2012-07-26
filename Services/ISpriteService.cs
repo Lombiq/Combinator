25cdfb950108887801ef4f1ef466e105e7940b91
Piedone <piedone@pyrocenter.hu>
2012-07-26 12:31:21 +0200

    Sprite service interface
    --HG--
    branch : image-sprites

---
+++ Services/ISpriteService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard;
+
+namespace Piedone.Combinator.Services
+{
+    public interface ISpriteService : IDependency
+    {
+
+    }
+}

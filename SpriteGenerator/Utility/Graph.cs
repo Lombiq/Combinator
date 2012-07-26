50f1355e62f6b563d85871919cecb8cf7ca755b2
Piedone <piedone@pyrocenter.hu>
2012-07-26 16:58:39 +0200

    Renaming DFSSequence argument to comply with C# naming conventions
    --HG--
    branch : image-sprites

--- SpriteGenerator/Utility/Graph.cs
+++ SpriteGenerator/Utility/Graph.cs
@@ -47,9 +47,9 @@ namespace Piedone.Combinator.SpriteGenerator.Utility
         /// <param name="node">Actual node.</param>
         /// <param name="visitedNodes">Visited nodes.</param>
         /// <param name="topologicalOrder">Topological order of the graph.</param>
-        /// <param name="DFSSequence">0-1 sequence representing of the DFS traversing. 0 means forth step,
+        /// <param name="dfsSequence">0-1 sequence representing of the DFS traversing. 0 means forth step,
         /// 1 means back step.</param>
-        private void VisitNode(int node, Dictionary<int, bool> visitedNodes, List<int> topologicalOrder, List<Bit> DFSSequence)
+        private void VisitNode(int node, Dictionary<int, bool> visitedNodes, List<int> topologicalOrder, List<Bit> dfsSequence)
         {
             //If node is not visited yet
             if (!visitedNodes[node])
@@ -59,7 +59,7 @@ namespace Piedone.Combinator.SpriteGenerator.Utility
                 //Adding node to topological order and adding a 0 to DFS sequense strores the forth step belongs 
                 //to the module
                 topologicalOrder.Add(node);
-                DFSSequence.Add(0);
+                dfsSequence.Add(0);
 
                 //Ordering neighbors by their weights
                 var orderedNeighbors = from neighbor in _nodes[node].OutgoingEdges.Keys
@@ -72,36 +72,36 @@ namespace Piedone.Combinator.SpriteGenerator.Utility
                 //Visiting nodes in the afore calculated order.
                 foreach (int neighbor in neighborsOfNode)
                 {
-                    VisitNode(neighbor, visitedNodes, topologicalOrder, DFSSequence);
+                    VisitNode(neighbor, visitedNodes, topologicalOrder, dfsSequence);
                 }
             }
 
             //If node and all of its DFS descendants are visited, it's needed to store the back step 
             //belonging to the module.
-            DFSSequence.Add(1);
+            dfsSequence.Add(1);
         }
 
         /// <summary>
         /// Ordered depth first search. Calculates module sequence and DFS sequence belonging to the graph. 
         /// These determines the O-Tree with the same orientation what the graph has.
         /// </summary>
-        /// <param name="DFSSequence">O-Tree DFSSequence.</param>
+        /// <param name="dfsSequence">O-Tree DFSSequence.</param>
         /// <returns>O-Tree ModuleSequence.</returns>
-        public List<int> DepthFirstSearch(List<Bit> DFSSequence)
+        public List<int> DepthFirstSearch(List<Bit> dfsSequence)
         {
             //Clearing DFS sequence and creating an empty list of module labels.
-            DFSSequence.Clear();
+            dfsSequence.Clear();
             List<int> nodeOrder = new List<int>();
             //Every node is unvisited yet.
             Dictionary<int, bool> visitedNodes = _nodes.Keys.ToDictionary(item => item, item => false);
 
             //Visit starts at root module representing by label -1.
-            VisitNode(-1, visitedNodes, nodeOrder, DFSSequence);
+            VisitNode(-1, visitedNodes, nodeOrder, dfsSequence);
 
             //Removing root module.
             nodeOrder.RemoveAt(0);
-            DFSSequence.RemoveAt(0);
-            DFSSequence.RemoveAt(DFSSequence.Count - 1);
+            dfsSequence.RemoveAt(0);
+            dfsSequence.RemoveAt(dfsSequence.Count - 1);
 
             return nodeOrder;
         }

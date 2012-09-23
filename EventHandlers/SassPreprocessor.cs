59f69a745d0e927315b8c47dad911d119e87bcf8
Piedone <piedone@pyrocenter.hu>
2012-09-23 23:50:19 +0200

    Adding SASS preprocessor Contribution of Onestop Internet, Inc. (http://www.onestop.com/)
    --HG--
    branch : image-sprites

---
+++ EventHandlers/SassPreprocessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Environment.Extensions;
+using Piedone.Combinator.Models;
+using System.IO;
+using SassAndCoffee.Ruby.Sass;
+using Orchard.FileSystems.VirtualPath;
+
+namespace Piedone.Combinator.EventHandlers
+{
+    [OrchardFeature("Piedone.Combinator.Sass")]
+    public class SassPreprocessor : ICombinatorResourceEventHandler
+    {
+        private readonly IVirtualPathProvider _virtualPathProvider;
+
+        public SassPreprocessor(IVirtualPathProvider virtualPathProvider)
+        {
+            _virtualPathProvider = virtualPathProvider;
+        }
+
+        public void OnContentLoaded(CombinatorResource resource)
+        {
+            var extension = Path.GetExtension(resource.RelativeUrl.ToString()).ToLowerInvariant();
+            if (extension != ".sass" && extension != ".scss") return;
+
+            using (var compiler = new SassCompiler())
+            {
+                resource.Content = compiler.Compile(_virtualPathProvider.MapPath(resource.RelativeVirtualPath).Replace("\\", @"\"), false, new List<string>());
+            }
+        }
+
+        public void OnContentProcessed(CombinatorResource resource)
+        {
+        }
+    }
+}

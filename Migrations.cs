a529feec7049ce09584f4099c231754d49f469a3
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2013-07-03 14:04:13 +0200

    Migrations code styling
    --HG--
    branch : Orchard 1.7

--- Migrations.cs
+++ Migrations.cs
@@ -29,7 +29,8 @@ namespace Piedone.Combinator.Migrations
                     .Column<string>("Type")
                     .Column<DateTime>("LastUpdatedUtc")
                     .Column<string>("Settings", column => column.Unlimited())
-            ).AlterTable(typeof(CombinedFileRecord).Name,
+                )
+            .AlterTable(typeof(CombinedFileRecord).Name,
                 table => table
                     .CreateIndex("File", new string[] { "HashCode" })
             );
@@ -48,7 +49,7 @@ namespace Piedone.Combinator.Migrations
                     .Column<string>("EmbedCssImagesStylesheetExcludeRegex", column => column.Unlimited())
                     .Column<bool>("GenerateImageSprites")
                     .Column<string>("ResourceSetRegexes", column => column.Unlimited())
-            );
+                );
 
 
             return 10;
@@ -73,12 +74,12 @@ namespace Piedone.Combinator.Migrations
             SchemaBuilder.AlterTable(typeof(CombinatorSettingsPartRecord).Name,
                 table => table
                     .AddColumn<string>("CombinationExcludeRegex")
-            );
+                );
 
             SchemaBuilder.AlterTable(typeof(CombinedFileRecord).Name,
                 table => table
                     .AddColumn<string>("Settings", column => column.Unlimited())
-            );
+                );
 
             return 3;
         }
@@ -103,7 +104,7 @@ namespace Piedone.Combinator.Migrations
             SchemaBuilder.AlterTable(typeof(CombinatorSettingsPartRecord).Name,
                 table => table
                     .AddColumn<string>("ResourceSetRegexes")
-            );
+                );
 
             return 5;
         }
@@ -113,7 +114,7 @@ namespace Piedone.Combinator.Migrations
             SchemaBuilder.AlterTable(typeof(CombinatorSettingsPartRecord).Name,
                 table => table
                     .AddColumn<bool>("EnableForAdmin")
-            );
+                );
 
             return 6;
         }

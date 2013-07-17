cff9b626788f979b24f4ae7f8c22bc7df0b74d41
Piedone <piedone@pyrocenter.hu>
2013-07-17 16:51:39 +0200

    Fixing tests

--- Piedone.Combinator.Tests/Services/CacheFileServiceTests.cs
+++ Piedone.Combinator.Tests/Services/CacheFileServiceTests.cs
@@ -228,6 +228,12 @@ namespace Piedone.Combinator.Tests.Services
             {
                 return CreatedFiles.Any(p => p.StartsWith(path));
             }
+
+
+            public string GetStoragePath(string url)
+            {
+                throw new NotImplementedException();
+            }
         }
 
         private class StubStorageFolder : IStorageFolder

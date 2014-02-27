042c2750be0224618809e421b7e464c0ef6943ee
Piedone <piedone@pyrocenter.hu>
2014-02-27 17:11:47 +0100

    Changing settings editor to make use of the technique where update only happens when the module's editor is posted Contribution of Lombiq Technologies (http://lombiq.com/)
    --HG--
    branch : Orchard 1.8

--- Drivers/CombinatorSettingsPartDriver.cs
+++ Drivers/CombinatorSettingsPartDriver.cs
@@ -45,91 +45,97 @@ namespace Piedone.Combinator.Drivers
         // GET
         protected override DriverResult Editor(CombinatorSettingsPart part, dynamic shapeHelper)
         {
-            return ContentShape("Parts_CombinatorSettings_SiteSettings",
-                    () => shapeHelper.EditorTemplate(
-                            TemplateName: "Parts.CombinatorSettings.SiteSettings",
-                            Model: part,
-                            Prefix: Prefix))
-                   .OnGroup("Combinator");
+            return Editor(part, null, shapeHelper);
         }
 
         // POST
         protected override DriverResult Editor(CombinatorSettingsPart part, IUpdateModel updater, dynamic shapeHelper)
         {
-            dynamic formerSettings = new ExpandoObject();
-            formerSettings.CombinationExcludeRegex = part.CombinationExcludeRegex;
-            formerSettings.CombineCdnResources = part.CombineCdnResources;
-            formerSettings.ResourceBaseUrl = part.ResourceBaseUrl;
-            formerSettings.MinifyResources = part.MinifyResources;
-            formerSettings.MinificationExcludeRegex = part.MinificationExcludeRegex;
-            formerSettings.EmbedCssImages = part.EmbedCssImages;
-            formerSettings.EmbeddedImagesMaxSizeKB = part.EmbeddedImagesMaxSizeKB;
-            formerSettings.EmbedCssImagesStylesheetExcludeRegex = part.EmbedCssImagesStylesheetExcludeRegex;
-            formerSettings.GenerateImageSprites = part.GenerateImageSprites;
-            formerSettings.ResourceSetRegexes = part.ResourceSetRegexes;
-
-            updater.TryUpdateModel(part, Prefix, null, null);
-
-
-            if (part.CombinationExcludeRegex != formerSettings.CombinationExcludeRegex
-                || part.CombineCdnResources != formerSettings.CombineCdnResources
-                || part.ResourceBaseUrl != formerSettings.ResourceBaseUrl
-                || part.MinifyResources != formerSettings.MinifyResources
-                || (part.MinifyResources && part.MinificationExcludeRegex != formerSettings.MinificationExcludeRegex)
-                || part.EmbedCssImages != formerSettings.EmbedCssImages
-                || (part.EmbedCssImages && part.EmbeddedImagesMaxSizeKB != formerSettings.EmbeddedImagesMaxSizeKB)
-                || (part.EmbedCssImages && part.EmbedCssImagesStylesheetExcludeRegex != formerSettings.EmbedCssImagesStylesheetExcludeRegex)
-                || part.GenerateImageSprites != formerSettings.GenerateImageSprites
-                || (part.ResourceSetRegexes != formerSettings.ResourceSetRegexes))
-            {
-                var valuesAreValid = true;
-
-                if (!string.IsNullOrEmpty(part.ResourceBaseUrl))
+            return ContentShape("Parts_CombinatorSettings_SiteSettings",
+                () =>
                 {
-                    try
+                    if (updater != null)
                     {
-                        UriHelper.CreateUri(part.ResourceBaseUrl);
+                        dynamic formerSettings = new ExpandoObject();
+                        formerSettings.CombinationExcludeRegex = part.CombinationExcludeRegex;
+                        formerSettings.CombineCdnResources = part.CombineCdnResources;
+                        formerSettings.ResourceBaseUrl = part.ResourceBaseUrl;
+                        formerSettings.MinifyResources = part.MinifyResources;
+                        formerSettings.MinificationExcludeRegex = part.MinificationExcludeRegex;
+                        formerSettings.EmbedCssImages = part.EmbedCssImages;
+                        formerSettings.EmbeddedImagesMaxSizeKB = part.EmbeddedImagesMaxSizeKB;
+                        formerSettings.EmbedCssImagesStylesheetExcludeRegex = part.EmbedCssImagesStylesheetExcludeRegex;
+                        formerSettings.GenerateImageSprites = part.GenerateImageSprites;
+                        formerSettings.ResourceSetRegexes = part.ResourceSetRegexes;
+
+                        updater.TryUpdateModel(part, Prefix, null, null);
+
+
+                        if (part.CombinationExcludeRegex != formerSettings.CombinationExcludeRegex
+                            || part.CombineCdnResources != formerSettings.CombineCdnResources
+                            || part.ResourceBaseUrl != formerSettings.ResourceBaseUrl
+                            || part.MinifyResources != formerSettings.MinifyResources
+                            || (part.MinifyResources && part.MinificationExcludeRegex != formerSettings.MinificationExcludeRegex)
+                            || part.EmbedCssImages != formerSettings.EmbedCssImages
+                            || (part.EmbedCssImages && part.EmbeddedImagesMaxSizeKB != formerSettings.EmbeddedImagesMaxSizeKB)
+                            || (part.EmbedCssImages && part.EmbedCssImagesStylesheetExcludeRegex != formerSettings.EmbedCssImagesStylesheetExcludeRegex)
+                            || part.GenerateImageSprites != formerSettings.GenerateImageSprites
+                            || (part.ResourceSetRegexes != formerSettings.ResourceSetRegexes))
+                        {
+                            var valuesAreValid = true;
+
+                            if (!string.IsNullOrEmpty(part.ResourceBaseUrl))
+                            {
+                                try
+                                {
+                                    UriHelper.CreateUri(part.ResourceBaseUrl);
+                                }
+                                catch (UriFormatException ex)
+                                {
+                                    valuesAreValid = false;
+                                    updater.AddModelError("Combinator.ResourceBaseUrlMalformed", T("The resource base URL you provided is invalid: {0}", ex.Message));
+                                }
+                            }
+
+                            if (!string.IsNullOrEmpty(part.CombinationExcludeRegex))
+                            {
+                                if (!TestRegex(part.CombinationExcludeRegex, T("combination exclude regex"), updater)) valuesAreValid = false;
+                            }
+                            if (!string.IsNullOrEmpty(part.MinificationExcludeRegex))
+                            {
+                                if (!TestRegex(part.MinificationExcludeRegex, T("minification exclude regex"), updater)) valuesAreValid = false;
+                            }
+                            if (!string.IsNullOrEmpty(part.EmbedCssImagesStylesheetExcludeRegex))
+                            {
+                                if (!TestRegex(part.EmbedCssImagesStylesheetExcludeRegex, T("embedded css images exclude regex"), updater)) valuesAreValid = false;
+                            }
+                            if (!string.IsNullOrEmpty(part.ResourceSetRegexes))
+                            {
+                                int i = 1;
+                                foreach (var regex in part.ResourceSetRegexesEnumerable)
+                                {
+                                    if (!TestRegex(regex, T("resource set regexes #{0}", i.ToString()), updater)) valuesAreValid = false;
+                                    i++;
+                                }
+                            }
+
+                            if (valuesAreValid)
+                            {
+                                // Not emptying the cache would cause inconsistencies
+                                _cacheFileService.Empty();
+                                _notifier.Information(T("Combinator cache emptied."));
+                            }
+                        }
+
+                        _combinatorEventHandler.ConfigurationChanged(); 
                     }
-                    catch (UriFormatException ex)
-                    {
-                        valuesAreValid = false;
-                        updater.AddModelError("Combinator.ResourceBaseUrlMalformed", T("The resource base URL you provided is invalid: {0}", ex.Message));
-                    } 
-                }
 
-                if (!string.IsNullOrEmpty(part.CombinationExcludeRegex))
-                {
-                    if (!TestRegex(part.CombinationExcludeRegex, T("combination exclude regex"), updater)) valuesAreValid = false;
-                }
-                if (!string.IsNullOrEmpty(part.MinificationExcludeRegex))
-                {
-                    if (!TestRegex(part.MinificationExcludeRegex, T("minification exclude regex"), updater)) valuesAreValid = false;
-                }
-                if (!string.IsNullOrEmpty(part.EmbedCssImagesStylesheetExcludeRegex))
-                {
-                    if (!TestRegex(part.EmbedCssImagesStylesheetExcludeRegex, T("embedded css images exclude regex"), updater)) valuesAreValid = false;
-                }
-                if (!string.IsNullOrEmpty(part.ResourceSetRegexes))
-                {
-                    int i = 1;
-                    foreach (var regex in part.ResourceSetRegexesEnumerable)
-                    {
-                        if (!TestRegex(regex, T("resource set regexes #{0}", i.ToString()), updater)) valuesAreValid = false;
-                        i++;
-                    }
-                }
-
-                if (valuesAreValid)
-                {
-                    // Not emptying the cache would cause inconsistencies
-                    _cacheFileService.Empty();
-                    _notifier.Information(T("Combinator cache emptied.")); 
-                }
-            }
-
-            _combinatorEventHandler.ConfigurationChanged();
-
-            return Editor(part, shapeHelper);
+                    return shapeHelper.EditorTemplate(
+                        TemplateName: "Parts.CombinatorSettings.SiteSettings",
+                        Model: part,
+                        Prefix: Prefix);
+                })
+                .OnGroup("Combinator");
         }
 
 

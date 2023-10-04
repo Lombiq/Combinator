37e25f31cd36b129add051ea4644d1f45db60b55
Ádám Jakab <adam.jakab@lombiq.com>
2023-10-04 18:38:32 +0200

    Emptying Combinator cache on shell activation if marker file exists; Adding validate-pull-request.yml to csproj

---
+++ EventHandlers/CacheFileEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using Orchard.Environment;
+using Orchard.Environment.Configuration;
+using Orchard.Environment.Extensions;
+using Orchard.FileSystems.AppData;
+using Piedone.Combinator.Constants;
+using Piedone.Combinator.Services;
+
+namespace Piedone.Combinator.EventHandlers
+{
+    [OrchardFeature("Piedone.Combinator")]
+    public class CacheFileEventHandler : IOrchardShellEvents
+    {
+        private const string ClearCacheFileName = FileNames.ClearCache + ".txt";
+
+        private readonly ICacheFileService _cacheFileService;
+        private readonly IAppDataFolder _appDataFolder;
+
+        private readonly string _basePath;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether emptying Combinator cache is disabled, primarily through HostComponents.config.
+        /// </summary>
+        public bool IsEmptyingCacheDisabled { get; set; }
+
+        public CacheFileEventHandler(ICacheFileService cacheFileService, IAppDataFolder appDataFolder, ShellSettings shellSettings)
+        {
+            _cacheFileService = cacheFileService;
+            _appDataFolder = appDataFolder;
+
+            _basePath = _appDataFolder.Combine(FolderNames.Sites, shellSettings.Name, FolderNames.PiedoneModules, FolderNames.Combinator);
+        }
+
+        public void Activated()
+        {
+            if (IsEmptyingCacheDisabled) return;
+
+            var pathToClearCacheFile = _appDataFolder.Combine(_basePath, ClearCacheFileName);
+
+            if (!_appDataFolder.FileExists(pathToClearCacheFile)) return;
+
+            _cacheFileService.Empty();
+
+            _appDataFolder.DeleteFile(pathToClearCacheFile);
+        }
+
+        public void Terminating()
+        {
+            // Terminating event does not need to be implemented.
+        }
+    }
+}
